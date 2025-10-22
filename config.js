bf1ae47fa91952f67c1d41bc748b6e06d71a14a5
MRSUNNY40 <sanwalkhan7709@gmail.com>
2025-10-22 11:05:38 +0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,1 +1,72 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "",
+// add your Session Id 
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+// make true or false status auto seen
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+// make true if you want auto reply on status 
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+// make true if you want auto reply on status 
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SᴇᴇN YᴏᴜʀE SᴛᴀᴛᴜS JᴜsT NᴏW 𝙈𝙍-𝙎𝙐𝙉𝙉𝙔 𓅓*",
+// set the auto reply massage on status reply
+AUTO_CHATBOT: https://i.ibb.co/Q7LsHPzH/shaban-md.jpg.AUTO_CHATBOT || "true", 
+// "true" = default ON, "false" = default OFF
+WELCOME: process.env.WELCOME || "false",
+// true if want welcome and goodbye msg in groups    
+ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
+// make true to know who dismiss or promoted a member in group  
+PREFIX: process.env.PREFIX || ".",
+// add your prifix for bot
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://i.ibb.co/Q7LsHPzH/sunny-md.jpg.io/mrshaban/Picsart_25-02-01_22-47-44-239.jpg",
+// add custom menu and mention reply image url   
+BOT_NAME: process.env.BOT_NAME || "SUNNY-MD V5",
+// add bot namw here for menu
+STICKER_NAME: process.env.STICKER_NAME || "𝙈𝙍-𝙎𝙐𝙉𝙉𝙔",
+// type sticker pack name 
+CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+// make this true for custum emoji react    
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+// chose custom react emojis by yourself  
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923030123272",
+// add your bot owner number
+OWNER_NAME: process.env.OWNER_NAME || "MR-SUNNY",
+// add bot owner name
+DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝙈𝙍  𝙎𝙐𝙉𝙉𝙔*",
+// add bot owner name    
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/Q7LsHPzH/sunny-md.jpg/Picsart_25-02-01_22-47-44-239.jpg",
+// add img for alive msg 
+READ_MESSAGE: process.env.READ_MESSAGE || "false",
+// Turn true or false for automatic read msgs
+AUTO_REACT: process.env.AUTO_REACT || "false",
+// make this true or false for auto react on all msgs  
+MODE: process.env.MODE || "public",
+// make bot public-private-inbox-group 
+ANTI_LINK: process.env.ANTI_LINK || "true",
+// make anti link true,false for groups  
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
+// make true or false automatic text reply 
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+// maks true for always online 
+PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+// make false if want private mod
+AUTO_TYPING: process.env.AUTO_TYPING || "false",
+// true for automatic show typing   
+READ_CMD: process.env.READ_CMD || "false",
+// true if want mark commands as read 
+DEV: process.env.DEV || "923030123272",
+//replace with your whatsapp number        
+ANTI_CALL: process.env.ANTI_CALL || "false",
+REJECT_MSG: process.env.REJECT_MSG || "*_SOORY MY BOSS IS BUSY PLEASE DONT CALL ME_*",
+ANTI_DELETE: process.env.ANTI_DELETE || "false",
+// set true false for anti delete
+ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox", 
+// change it to 'same' if you want to resend deleted message in same chat
+AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+// make it true for auto recoding 
+};
